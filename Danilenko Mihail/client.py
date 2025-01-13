1fabd3ccc527cfa1bf16116dbe0eae6959545088
kiril <ioje3f@>
2025-01-13 07:55:48 +0300

    commit 1

---
+++ Danilenko Mihail/client.py
@@ -0,0 +1,70 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import database
+
+ADMIN_ID = 406675702
+CHAT_ID = 1001934227821
+MY_ID = 852293214
+bot = Client(name="AutoReplyTurboCars",
+             api_id=19584363,
+             api_hash="c8247fbf45c56ca820c5b44b3ae78abf",
+             phone_number="89853470136")
+
+
+#хендлер входящих сообщений от пользователя
+@bot.on_message()
+async def message_handler(client: Client, message: Message):
+    if message.from_user.is_bot or message.from_user.id == MY_ID:
+        return
+    check = database.check_autoreply(MY_ID)
+    if check:
+        check_is_first = database.check_is_first(message.chat.id, MY_ID)
+        print(check_is_first)
+        if not check_is_first:
+            database.add_users(message.chat.id, MY_ID)
+            with open("text_reply.txt", mode="r") as file:
+                text = file.read()
+            await client.send_message(chat_id=message.chat.id, text=text)
+        elif check_is_first == ["True"]:
+            with open("text_reply.txt", mode="r", encoding="utf-8") as file:
+                text = file.read()
+            await client.send_message(chat_id=message.chat.id, text=text)
+        if message.text:
+            await client.send_message(chat_id=CHAT_ID, text=message.text)
+            await client.send_message(chat_id=CHAT_ID, text=f"Пересланно от {message.from_user.username}")
+        elif message.video:
+            await client.send_video(chat_id=CHAT_ID, video=message.video.file_id)
+            await client.send_message(chat_id=CHAT_ID, text=f"Пересланно от {message.from_user.username}")
+        elif message.audio:
+            await client.send_audio(chat_id=CHAT_ID, audio=message.audio.file_id)
+            await client.send_message(chat_id=CHAT_ID, text=f"Пересланно от {message.from_user.username}")
+        elif message.photo:
+            await client.send_photo(chat_id=CHAT_ID, photo=message.photo.file_id)
+            await client.send_message(chat_id=CHAT_ID, text=f"Пересланно от {message.from_user.username}")
+        elif message.document:
+            await client.send_document(chat_id=CHAT_ID, document=message.document.file_id)
+            await client.send_message(chat_id=CHAT_ID, text=f"Пересланно от {message.from_user.username}")
+
+@bot.on_message(filters.reply)
+async def reply_handler(client: Client, message: Message):
+    if message.chat.id != CHAT_ID or message.from_user.id != ADMIN_ID:
+        return
+
+    if message.text == "+":
+        database.del_autoreply(MY_ID)
+        database.zeroing_users(MY_ID)
+        await client.send_message(chat_id=CHAT_ID, text="🚫Автоответ ОТКЛЮЧЕН")
+    if message.text[0] == "-":
+        text = message.text[1::]
+        database.add_autoreply(MY_ID)
+        with open("text_reply.txt", mode="w", encoding="utf-8") as file:
+            file.write(text)
+        await client.send_message(chat_id=CHAT_ID, text=f"✅Автоответ ЗАПУЩЕН\nтеперь автоответ:\n{text}")
+
+
+try:
+    bot.run()
+except Exception as ex:
+    print(ex)
+
+
---
+++ Shashurin Andrey/database.py
@@ -0,0 +1,59 @@
+import sqlite3
+
+
+def add_autoreply(user_id) -> None:
+    with sqlite3.connect("../database.db") as db:
+        cursor = db.cursor()
+        query = "INSERT INTO Profi (id, mod) VALUES (?, ?);"
+        cursor.execute(query, (user_id, "True"))
+        db.commit()
+
+
+
+def del_autoreply(user_id: int) -> None:
+    with sqlite3.connect("../database.db") as db:
+        cursor = db.cursor()
+        query = "DELETE FROM Profi WHERE id = ?;"
+        cursor.execute(query, (user_id, ))
+        db.commit()
+
+
+def check_autoreply(user_id: int):
+    with sqlite3.connect("../database.db") as db:
+        cursor = db.cursor()
+        query = "SELECT mod FROM Profi WHERE id = ?;"
+        cursor.execute(query, (user_id, ))
+        data = cursor.fetchone()
+        db.commit()
+        return data
+
+
+def check_is_first(user_id, id_profi):
+    with sqlite3.connect("../database.db") as db:
+        cursor = db.cursor()
+        query = "SELECT is_first FROM Users WHERE id = ? AND id_profi = ?;"
+        cursor.execute(query, (user_id, id_profi))
+        data = cursor.fetchone()
+        db.commit()
+        return data
+
+def add_users(user_id, id_profi) -> None:
+    with sqlite3.connect("../database.db") as db:
+        cursor = db.cursor()
+        query = "INSERT INTO Users (id, is_first, id_profi) VALUES (?, ?, ?);"
+        cursor.execute(query, (user_id, "True", id_profi))
+        db.commit()
+
+
+def zeroing_users(user_id, id_profi):
+    with sqlite3.connect("../database.db") as db:
+        cursor = db.cursor()
+        query = "UPDATE Users SET is_first = ? WHERE id_profi = ?;"
+        cursor.execute(query, ("FALSE", id_profi))
+        db.commit()
+
+
+
+
+
+
